0409800ed4e915e20a829ce3feb5d6fd18bd230f
James Robinson <jrobind@outlook.com>
2018-01-27 11:04:24 +0000

    add error handling

--- isPrototypeOf.js
+++ isPrototypeOf.js
@@ -1,6 +1,13 @@
 (function() {
     
   function isPrototypeOf(originalObj, objCheck) {
+    if (typeof originalObj !== 'object') {
+      throw Error('First parameter must be an object');
+    }
+    if (typeof objCheck !== 'object') {
+      throw Error('Second parameter must be an object');
+    } 
+      
     var proto = Object.getPrototypeOf(objCheck);
     // base case
 	if (proto === originalObj) {
